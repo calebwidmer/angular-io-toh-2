22047ddb9e6ca628aa69b1d5096b123eea5ce497
calebwidmer <calebwidmer@gmail.com>
2022-02-10 14:39:26 -0500

    2. Display a List Complete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../interfaces/hero';
+import { Hero } from '../interfaces/hero';
 import { HEROES } from '../mockData/mock-heroes';
-
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  heroes:Hero[] = HEROES;
- 
+
+  heroes = HEROES;
+  selectedHero?: Hero;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
