363894dbef38f354d562218a06844cfbf73fb461
calebwidmer <calebwidmer@gmail.com>
2022-02-10 15:56:03 -0500

    3 Create a feature component -complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule,Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';  
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component'; 
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; 
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
