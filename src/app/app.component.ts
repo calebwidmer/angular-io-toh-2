18c706410e3fc14601e41f24406972a5287a776b
calebwidmer <calebwidmer@gmail.com>
2022-02-10 13:41:48 -0500

    2. Display a List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 
 @Component({
@@ -8,6 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  public constructor(private titleService: Title) { }
+  
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(this.title);
+  }
+ 
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule,Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';  
-import { AppComponent } from './app.component'; 
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component'; 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroesComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
